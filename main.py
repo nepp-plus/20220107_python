6c118a5d12e76a550d81f2fc64f0ec90087e6b39
nepp-plus <nepp.plus@gmail.com>
2022-01-07 16:49:30 +0900

    실제 FCM 서버에 post 요청

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # FCM 서버에 -> 파이썬 프로그램으로 -> API 호출 (기기 푸시알림 전송 요청)
 import requests
+import json
 
 # 함수 제작 : 보내줄 문구 / 본문 내용을 받아서 전송.
 
@@ -7,12 +8,12 @@ def send_fcm_notificaion(title, body):
     
     # FCM 서버에 요청
     # 1. 호스트 주소 / 기능 주소 => 실제 기능 URL
-    url = 'https://fcm.googleapis.com/fcm/send'
+    fcm_url = 'https://fcm.googleapis.com/fcm/send'
     
     # 2. 첨부해야할 파라미터 (보내줄 데이터)
     
     # 헤더에, 인증키 (token) 를 담아서 전달.
-    headers = {
+    fcm_headers = {
         'Authorization':'key=AAAAcyMDrYk:APA91bFfMLRhrFsm8gE0i07RAbnM7_JsD4CmrMoY-Uswh70tQFpPrPm9KHEvG_tNUJ44sgy0SRq0yRcfk_yhyIvsGEGPP5GRW2b6JjfXNyrrXRz1u9SEtxXXtDejjtdaXo4Eay1UnPO5',
         'Content-Type': 'application/json; UTF-8',
     }
@@ -27,5 +28,7 @@ def send_fcm_notificaion(title, body):
         }, # 기본양식의 알림.  data-message 로 보내면 : 커스터마이징 지원 알림.
     }
     
-    # 3. 어떤 방식 
+    # 3. 어떤 방식  + 실제 API 호출.
+    
+    requests.post(fcm_url, data=json.dumps(content), headers=fcm_headers)
     
